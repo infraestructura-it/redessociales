9f65ff473059a73552c2c256165ad949e1ff5973
Jairo Sepulveda C <30697670+infraestructura-it@users.noreply.github.com>
2025-02-17 20:05:36 -0500

    cambios sitio y workflows

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+  site:'https://github.io/infraestructura-it/redessociales',
   integrations: [],
   markdown: {
     shikiConfig: {
